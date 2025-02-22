0b7bede45cf970e2707b2e01644888385eb39f1f
Shubham <shubhambrown1@gmail.com>
2025-02-22 19:45:13 +0530

    Updated artist pages and scripts

--- script.js
+++ script.js
@@ -1,49 +1,49 @@
- // Select all video and audio elements
- const audioElements = document.querySelectorAll(".audio");
- const videoElements = document.querySelectorAll(".video");
+// Select all video and audio elements
+const audioElements = document.querySelectorAll(".audio");
+const videoElements = document.querySelectorAll(".video");
 
- // Add event listeners to each audio element
- audioElements.forEach((audio) => {
-   audio.addEventListener("play", () => {
-     // Pause all other audio elements when one is played
-     audioElements.forEach((otherAudio) => {
-       if (otherAudio !== audio) {
-         otherAudio.pause();
-       }
-     });
-     // Pause all videos when audio is played
-     videoElements.forEach((video) => {
-       video.pause();
-     });
-   });
- });
+// Add event listeners to each audio element
+audioElements.forEach((audio) => {
+  audio.addEventListener("play", () => {
+    // Pause all other audio elements when one is played
+    audioElements.forEach((otherAudio) => {
+      if (otherAudio !== audio) {
+        otherAudio.pause();
+      }
+    });
+    // Pause all videos when audio is played
+    videoElements.forEach((video) => {
+      video.pause();
+    });
+  });
+});
 
- // Add event listeners to each video element
- videoElements.forEach((video) => {
-   video.addEventListener("play", () => {
-     // Pause all other video elements when one is played
-     videoElements.forEach((otherVideo) => {
-       if (otherVideo !== video) {
-         otherVideo.pause();
-       }
-     });
-     // Pause all audios when a video is played
-     audioElements.forEach((audio) => {
-       audio.pause();
-     });
-   });
- });
+// Add event listeners to each video element
+videoElements.forEach((video) => {
+  video.addEventListener("play", () => {
+    // Pause all other video elements when one is played
+    videoElements.forEach((otherVideo) => {
+      if (otherVideo !== video) {
+        otherVideo.pause();
+      }
+    });
+    // Pause all audios when a video is played
+    audioElements.forEach((audio) => {
+      audio.pause();
+    });
+  });
+});
 
- document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
   let videos = document.querySelectorAll("video");
 
-  videos.forEach(video => {
-      video.addEventListener("play", function () {
-          videos.forEach(v => {
-              if (v !== video) {
-                  v.pause();
-              }
-          });
+  videos.forEach((video) => {
+    video.addEventListener("play", function () {
+      videos.forEach((v) => {
+        if (v !== video) {
+          v.pause();
+        }
       });
+    });
   });
 });
